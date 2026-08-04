eb8c8d0dcdd7a8192dd0c9f33a8e29cfbdef0d48
sid <mhaskesudhir55@gmail.com>
2026-08-04 13:10:59 +0530

    Add Jest coverage for SonarQube

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,18 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
 module.exports = {
+  preset: "ts-jest",
   testEnvironment: "node",
-  transform: {
-    ...tsJestTransformCfg,
-  },
+
+  collectCoverage: true,
+
+  coverageDirectory: "coverage",
+
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/tests/**"
+  ],
+
+  coverageReporters: [
+    "text",
+    "lcov"
+  ]
 };
